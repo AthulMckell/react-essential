a2927ad594507f743e422a1a436b32b670d979a6
Athul Raj <athulraj786@gmail.com>
2022-04-09 13:14:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { useState, useEffect} from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//https://api.github.com/users/AthulMckell
+
+function App({ login }) {
+  const [data, setData ] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() =>{
+    fetch(`https://api.github.com/users/${ login }`)
+    .then((response)=> response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  },[login]);
+
+  if(loading) return <h1>loading....</h1>
+  if(error)
+    return <pre> { JSON.stirngify(error, null, 2) } </pre>;
+  if(!data) return null;
+
+    return (<div>
+      <h1>{data.name}</h1>
+      {/* <p>{data.location}</p> */}
+      <img alt={data.login} src={data.avatar_url} />
+      </div>
+    );
+
+  
+  return <div>No user available</div>;
+    
 }
 
 export default App;
+
+
+// function Header(props){
+//   return (
+//     <header>
+//       <h1>{props.name} Workshop</h1>
+//     </header>
+//   );
+// }
+
+// function Main(props){
+//   return(
+//   <section>
+//     <p>We Forge in {props.adj} fire, the weapon of Gods</p>
+//     <img 
+//     src={trav}
+//     height={250}
+//     alt='an image of a extreme sport' />
+//     <ul style={{textAlign:"left"}}>
+//       {props.dishes.map((dish)=>
+//        <li key={dish.id}>{dish.title}</li>)}
+//     </ul>
+//   </section>
+//   );
+// }
+
+// function Footer(props){
+//   return (
+//     <section>
+//       <p>
+//         Have a {props.ch} day.
+//       </p>
+//       <p>Copyright {props.ct}</p>
+//     </section>
+//   );
+// }
+
+// const dishes = [
+//   "Armegeddon from hell",
+//   "Thors hammer",
+//   "iron suit"
+// ];
+
+// const dishObjects = dishes.map((dish, i) => ({ id: i, title: dish }));
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App login='AthulMckell' />,
+   document.getElementById('root'));
+   
+
+// React.createElement("h1",{style: {color : "blue"}}, "HEYYYY EVERYONE!")
+  // <ul>
+  //   <li>MONDAY-</li>
+  //   <li>Tuesday</li>
+  //   <li>Wednesday</li>
+  // </ul>
+  // function AppTwo() {
+//   return <h1>This is the second app</h1>;
+// }
